f5991b9f5bb685a59e477a23ce5800021fa54eb7
amitpart96 <amitpartuk96@gmail.com>
2022-10-26 15:04:38 +0300

    new commit

---
+++ napari_hello/__init__.py
@@ -0,0 +1,23 @@
+import napari
+import napari_hello
+from napari_hello import run
+from napari.utils.notifications import show_info
+from skimage.io import imread
+
+
+def main():
+    napari
+
+
+if __name__ == "__main__":
+    main()
+
+
+def show_hello_message():
+    show_info('Hello, world!')
+
+
+def runnnnn():
+    #final_project
+    run.main()
+    show_info('Hello, run!')
